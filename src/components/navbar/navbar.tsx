cb64971216ec2460c240b1062ed1aad3df9f1b81
Ablrt <albert_amadanyan@edu.aua.am>
2023-08-29 20:47:24 +0400

    some cleanup

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <div className="navBar">
-      <div className="stocks">
-        <Link to="/">Stocks</Link>
+      <div className="name">
+        <Link to="/">Name</Link>
       </div>
       <div className="favourites">
         <Link to="/favourites">Favourite</Link>
       </div>
-      <div className="cart">
-        <Link to="/cart">Cart</Link>
+      <div className="fact">
+        <Link to="/cart">Cat Fact</Link>
       </div>
     </div>
   );
--- src/components/zibil/zibil.tsx
+++ src/components/zibil/zibil.tsx
@@ -1,30 +1,31 @@
 import "./zibil.css";
 import { useEffect, useState } from "react";
 import { changeFact } from "../../redux/catFactSlice";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
+import axios from "axios";
 
 const Stock = () => {
   const [name, setName] = useState("default");
-  const [value, setValue] = useState("")
-  const handleSubmit = (e:React.FormEvent) => {
+  const [value, setValue] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
     setName(value);
-    e.preventDefault()
+    e.preventDefault();
   };
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
   };
   useEffect(() => {
     console.log(name);
   }, [name]);
   return (
-    <div className="zibil stocks">
-      <h1>Stock</h1>
+    <div className="zibil name">
+      <h1>Name</h1>
       <h1>{name}</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" onChange={handleChange}/>
+          <input type="text" name="name" onChange={handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -35,19 +36,19 @@ const Stock = () => {
 const Favourite = () => {
   return (
     <div className="zibil favorite">
-      <h1>Favor</h1>
+      <h1>Favorite</h1>
     </div>
   );
 };
 
 const Cart = () => {
-  const fact = useSelector((state:RootState)=> state.catFact.value)
+  const fact = useSelector((state: RootState) => state.catFact.value);
   const dispatch = useDispatch();
 
   const handleClick = async () => {
-    const response = await fetch("https://catfact.ninja/fact");
-    const data = await response.json();
-    dispatch(changeFact(data.fact));
+    const response = await axios.get("https://catfact.ninja/fact");
+    const fact = await response.data.fact;
+    dispatch(changeFact(fact));
   };
   return (
     <div className="zibil cart">
